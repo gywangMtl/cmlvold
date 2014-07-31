25a65fbfa40423628b0fcd4538fc62ee92ac7ea6
Guangyi <guangyiwang@gmail.com>
2014-07-30 23:30:31 -0700

    add unix lib

--- storeClientFactory.ml
+++ storeClientFactory.ml
@@ -1,11 +1,14 @@
+open Unix;;
 
 type t = 
     {
         store_name:string;
+        sock:file_descr;
     };;
 
-let get_store_client s = {store_name = s};;
+let get_store_client s = {store_name = s;
+                          sock = stdout};;
 
-let close t = () ;;
+let close t = close t.sock;;
 
 let to_string t = String.concat "store name"  [t.store_name];;
